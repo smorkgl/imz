9429a2c156315f3dc76649704c5ff32f3396800b
smorkgl <48619197+smorkgl@users.noreply.github.com>
2024-02-05 20:37:32 +0300

    Update Wherewe.jsx

--- src/components/Wherewe.jsx
+++ src/components/Wherewe.jsx
@@ -1,12 +1,18 @@
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 export default function Wherewe() {
+  
     return (
-<YMaps>
-  <Map defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 }}>
+      <div className='flex'>
+        <div>ИМЗ находится:</div>
+        <div>
+          <YMaps>
+  <Map  width={"100%"} height={"100%"} defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 }}>
     <Placemark defaultGeometry={[55.751574, 40.283856]} />
   </Map>
 </YMaps>
+</div>
+</div>
     )
 }
 
