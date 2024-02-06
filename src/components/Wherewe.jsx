8203af655b020a12319799d8d1c3d3022d5dc56a
smorkgl <48619197+smorkgl@users.noreply.github.com>
2024-02-06 20:55:56 +0300

    123
    123

--- src/components/Wherewe.jsx
+++ src/components/Wherewe.jsx
@@ -3,14 +3,11 @@ import { YMaps, Map } from '@pbe/react-yandex-maps';
 export default function Wherewe() {
   
     return (
-      <div className='space-x-4 py-3 2xl max-w-7xl width-full mx-auto container mt-5 relative flex justify-between'>
-        <div>ИМЗ находится:</div>
-          <YMaps>
-  <Map         width="70vw"
-        height="70vh" defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 } }>
-  </Map>
-</YMaps>
-</div>
+      <YMaps>
+      <Map         width="100vw"
+            height="80vh" defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 } }>
+      </Map>
+    </YMaps>
     )
 }
 
