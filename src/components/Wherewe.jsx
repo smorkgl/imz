1a84bbd64ceb390e7e34189dad56df6d15c60002
smorkgl <catosrak@gmail.com>
2024-02-06 16:57:46 +0300

    123
    123

--- src/components/Wherewe.jsx
+++ src/components/Wherewe.jsx
@@ -1,17 +1,15 @@
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 
 export default function Wherewe() {
   
     return (
-      <div className='flex'>
+      <div className='space-x-4 py-3 2xl max-w-7xl width-full mx-auto container mt-5 relative flex justify-between'>
         <div>ИМЗ находится:</div>
-        <div>
           <YMaps>
-  <Map  width={"100%"} height={"100%"} defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 }}>
-    <Placemark defaultGeometry={[55.751574, 40.283856]} />
+  <Map         width="70vw"
+        height="70vh" defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 } }>
   </Map>
 </YMaps>
-</div>
 </div>
     )
 }
