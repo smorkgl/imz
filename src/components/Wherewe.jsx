349925ee0f1654a62c24ce5ecc1b3d82793ef01d
smorkgl <catosrak@gmail.com>
2024-02-07 17:19:29 +0300

    123
    1231

--- src/components/Wherewe.jsx
+++ src/components/Wherewe.jsx
@@ -2,14 +2,6 @@ import { YMaps, Map } from '@pbe/react-yandex-maps';
 
 export default function Wherewe() {
   
-<<<<<<< Updated upstream
-    return (
-      <YMaps>
-      <Map         width="100vw"
-            height="80vh" defaultState={{ center: [48.04806331746245, 40.26770622452088], zoom: 14 } }>
-      </Map>
-    </YMaps>
-=======
     return (  <div className='relative'>
           <YMaps>
   <Map         width=""
@@ -22,7 +14,6 @@ export default function Wherewe() {
 </div>
 </div>
 </div>
->>>>>>> Stashed changes
     )
 }
 
