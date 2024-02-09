0d77702cdf461b5308c8af5f357b62de0a181ea9
smorkgl <catosrak@gmail.com>
2024-02-09 15:51:33 +0300

    123
    123

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import TopMain from './components/TopMain'
 import Main from './components/Main'
 import Job from './components/Job'
 import Wherewe from './components/Wherewe'
+import Preloader from './components/Preloader'
+import {useEffect, useState} from 'react';
+
 
 function App() {
+const [isLoading, setIsLoading] = useState(true)
+useEffect(() => {
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1);
+})
 
-  return (
-    <div className="font-['Ubuntu']">
+  return ( 
+    <div>
+{isLoading ? (<Preloader/>) : (<div className="font-['Ubuntu']">
   <Header />
   <Nav />
   <Carousel />
@@ -21,7 +31,10 @@ function App() {
   <Wherewe/>
   <Footer />
   
-    </div>
+    </div>) }
+
+
+</div>
   )
 }
 
--- src/components/Wherewe.jsx
+++ src/components/Wherewe.jsx
@@ -2,16 +2,15 @@ import { YMaps, Map } from '@pbe/react-yandex-maps';
 
 export default function Wherewe() {
   
-    return (  <div className='relative'>
+    return (
+      <div className='max-w-5xl width-full mx-auto container'>
           <YMaps>
-  <Map         width=""
-        height="100vh" defaultState={{ center: [48.04506331746245, 40.24270622452088], zoom: 14.5 } }>
+  <Map         width="100vv"
+        height="80vh" defaultState={{ center: [48.04506331746245, 40.24270622452088], zoom: 14.5 } }>
   </Map>
 </YMaps>
-<div className='wherewe-box absolute left-0 top-0 h-full w-2/5 bg-blue-800 text-white flex place-items-center w-100'>
-<div className='mx-auto w-96'>
-  <h1>ИМЗ находится на очень оживленном участке трассы М4</h1>
-</div>
+<div>
+  test
 </div>
 </div>
     )
