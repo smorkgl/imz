e39f904e04ee1859ee4afef9334e1080544e0a2b
smorkgl <48619197+smorkgl@users.noreply.github.com>
2024-03-20 20:40:43 +0300

    123
    123

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -42,6 +42,10 @@ export default function Main() {
   const newsToShow = posts.items.slice(0, 4);
   const reverseNewsToShow = newsToShow.slice().reverse();
 
+  const isPostsLoading = posts.status === "loading";
+
+  const items = [1, 2, 3, 4];
+
   return (
     <div className="space-x-4 py-3 2xl max-w-7xl width-full mx-auto container mt-5 relative">
       <div className="flex justify-between">
@@ -65,115 +69,121 @@ export default function Main() {
           </div>
           <div className="work__line2 mt-2" />
           <div className="flex flex-wrap gap-20 mt-5">
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow animate-pulse">
-              <div className="relative">
-                <div>
-                  <img class="rounded-t-lg min-h-72 w-screen bg-gray-300" />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="currentColor"
-                    class="w-12 h-12 text-gray-500"
+            {isPostsLoading
+              ? items.map((item, index) => (
+                  <div
+                    key={index}
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow animate-pulse"
                   >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-              <div class="p-5">
-                <div class="block w-56 h-3 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-3 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-3 mb-4 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
-                  &nbsp;
-                </div>
-                <div class="block h-10 mt-4 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-lg text-inherit w-40">
-                  &nbsp;
-                </div>
-              </div>
-            </div>
-            {reverseNewsToShow.map((news) => (
-              <div key={news.id}>
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow relative">
-                  <Link to={`/news/${news.id}`}>
-                    <div
-                      className="relative"
-                      onMouseEnter={() => handleMouseEnterNews(news.id)}
-                      onMouseLeave={() => handleMouseLeaveNews(news.id)}
-                    >
-                      <img
-                        class="rounded-t-lg min-h-72"
-                        src={`http://localhost:3131/${news.imageUrl}`}
-                        alt={news.title}
-                      />
-                      <div className="absolute top-0 right-0 m-2 bg-white px-4 py-2 rounded-md font-bold text-blue-800">
-                        {news.date}
-                      </div>
-                      {isDropdownVisibleNews[news.id] && (
-                        <div className="absolute top-0 bg-black/50 h-full w-full flex items-center justify-center rounded-lg">
-                          <img
-                            src={next}
-                            className="img__next w-20 animate-jump-in"
-                          />
-                        </div>
-                      )}
-                    </div>
-                  </Link>
-                  <div class="p-5">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                      {news.title}
-                    </h5>
-                    <p class="mb-3 font-normal text-gray-700">
-                      {news.mini_title}
-                    </p>
-                    <Link
-                      to={`/news/${news.id}`}
-                      class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                    >
-                      Продолжение
-                      <svg
-                        class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 10"
-                      >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                    <div className="relative">
+                      <div className="relative">
+                        <img class="rounded-t-lg min-h-72 w-screen bg-gray-300" />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
                           stroke-width="2"
-                          d="M1 5h12m0 0L9 1m4 4L9 9"
-                        />
-                      </svg>
-                    </Link>
+                          stroke="currentColor"
+                          class="w-12 h-12 text-gray-500 svg__in-image "
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="p-5">
+                      <div class="block w-56 h-3 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-3 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-3 mb-4 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block w-56 h-2 mb-2 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-full text-inherit w-full">
+                        &nbsp;
+                      </div>
+                      <div class="block h-10 mt-4 font-sans text-5xl antialiased font-semibold leading-tight tracking-normal bg-gray-300 rounded-lg text-inherit w-40">
+                        &nbsp;
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                ))
+              : reverseNewsToShow.map((news) => (
+                  <div key={news.id}>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow relative">
+                      <Link to={`/news/${news.id}`}>
+                        <div
+                          className="relative"
+                          onMouseEnter={() => handleMouseEnterNews(news.id)}
+                          onMouseLeave={() => handleMouseLeaveNews(news.id)}
+                        >
+                          <img
+                            class="rounded-t-lg min-h-72"
+                            src={`http://localhost:3131/${news.imageUrl}`}
+                            alt={news.title}
+                          />
+                          <div className="absolute top-0 right-0 m-2 bg-white px-4 py-2 rounded-md font-bold text-blue-800">
+                            {news.date}
+                          </div>
+                          {isDropdownVisibleNews[news.id] && (
+                            <div className="absolute top-0 bg-black/50 h-full w-full flex items-center justify-center rounded-lg">
+                              <img
+                                src={next}
+                                className="img__next w-20 animate-jump-in"
+                              />
+                            </div>
+                          )}
+                        </div>
+                      </Link>
+                      <div class="p-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                          {news.title}
+                        </h5>
+                        <p class="mb-3 font-normal text-gray-700">
+                          {news.mini_title}
+                        </p>
+                        <Link
+                          to={`/news/${news.id}`}
+                          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        >
+                          Продолжение
+                          <svg
+                            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 10"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M1 5h12m0 0L9 1m4 4L9 9"
+                            />
+                          </svg>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
 
             <Link
               to={`/news`}
