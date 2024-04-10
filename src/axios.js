a3cc5f17f60024824cf0ad02ddf6de455f921c20
smorkgl <48619197+smorkgl@users.noreply.github.com>
2024-04-10 18:59:18 +0300

    123
    123

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
+<<<<<<< Updated upstream
   baseURL: "http://localhost:3131/",
+=======
+  baseURL: "https://imz-server.vercel.app",
+>>>>>>> Stashed changes
 });
 
 instance.interceptors.request.use((config) => {
