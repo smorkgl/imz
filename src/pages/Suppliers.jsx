e3f0c364314a23f65507bbe8142df9902e73f771
smorkgl <48619197+smorkgl@users.noreply.github.com>
2024-08-19 12:22:49 +0300

    123
    123

--- src/pages/Suppliers.jsx
+++ src/pages/Suppliers.jsx
@@ -1,12 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import TopMain from "../components/TopMain";
 import Breadcrumb from "../components/Breadcrumb";
+import emailjs from "@emailjs/browser";
 
 export default function Suppliers() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_a6z0367", "template_63iyekb", form.current, {
+        publicKey: "kifTa7VFekqtXUeFz",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+
   return (
     <div className="font-['Ubuntu']">
       <Header />
@@ -43,9 +64,9 @@ export default function Suppliers() {
           <div className="md:mt-5 md:m-0 my-6 ml-10">
             <div className="md:w-full bg-white shadow-md rounded rounded-lg p-5  w-3/4">
               <form
+            ref={form}
+              onSubmit={sendEmail}
                 id="contactForm"
-                action="#"
-                method="POST"
                 className="md:flex md:flex-col md:gap-5 grid grid-cols-1 sm:grid-cols-2 gap-6"
               >
                 <div>
@@ -92,23 +113,23 @@ export default function Suppliers() {
                 </div>
                 <div>
                   <label
-                    htmlFor="email"
+                    htmlFor="tel"
                     className="block text-gray-700 font-semibold mb-2"
                   >
                     Номер телефона
                   </label>
                   <input
-                    type="email"
-                    id="email"
-                    name="email"
+                    type="tel"
+                    id="tel"
+                    name="tel"
                     placeholder="Введите свой номер телефона"
                     className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-red-500 focus:border-red-500"
                   />
                   <div
-                    id="emailValidation"
+                    id="telValidation"
                     className="hidden text-red-600 text-sm"
                   >
-                    Пожалуйста введите свой ящик электронной почты.
+                    Пожалуйста введите свой номер телефона.
                   </div>
                 </div>
 
@@ -130,7 +151,7 @@ export default function Suppliers() {
                     id="subjectValidation"
                     className="hidden text-red-600 text-sm"
                   >
-                    Please enter the subject.
+                    Пожалуйста введите свою тему обращения
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -156,7 +177,8 @@ export default function Suppliers() {
                 </div>
                 <div className="sm:col-span-2">
                   <button
-                    type="submit"
+                       type="submit"
+                  value="Send"
                     className="w-full bg-red-700 text-white font-semibold py-2 px-4 rounded-md hover:bg-red-900 transition duration-300"
                   >
                     Отправить
